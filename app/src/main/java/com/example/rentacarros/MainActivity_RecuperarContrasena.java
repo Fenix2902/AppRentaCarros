893d05d22847fa40b9d5945ceb4a4c2c4cb382f5
Fenix2902 <alexandercalleagudelo@gmail.com>
2023-06-01 12:19:04 -0500

    se termina diseño de recuperar contraseña y se empieza a realizar programacion del boton actualizar

--- app/src/main/java/com/example/rentacarros/MainActivity_RecuperarContrasena.java
+++ app/src/main/java/com/example/rentacarros/MainActivity_RecuperarContrasena.java
@@ -3,12 +3,36 @@ package com.example.rentacarros;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity_RecuperarContrasena extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    EditText etPalabraClave, etnewpassword, etconfirmarpassword;
+    Button btnupdatepassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_recuperar_contrasena);
+        getSupportActionBar().hide();
+
+        etPalabraClave = findViewById(R.id.etpalabraclave);
+        etnewpassword = findViewById(R.id.etNewPassword);
+        etconfirmarpassword = findViewById(R.id.etConfirmarPassword);
+
+        btnupdatepassword = findViewById(R.id.btnUpdatePassword);
+
+        btnupdatepassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
